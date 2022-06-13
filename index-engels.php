b1e864a6f6fd5090ba133816db3db8cf0fbf1453
Halil Dogan <33289@ma-web.nl>
2022-06-13 20:09:37 +0200

    analystic en index engels en index nederlands veranderd

--- index-engels.php
+++ index-engels.php
@@ -27,17 +27,17 @@ $result = $connection->query('SELECT * FROM `opdrachten`');
     <script src="js/main.js" defer></script>
     <script src=".vscode/settings.json" defer></script>
     <script src="js/speak.js" defer></script>
-    <script>
-        (function (i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date(); a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+    <script src="js/analystic.js" defer></script>
 
-        ga('create', 'UA-XXXXX-Y', 'auto');
-        ga('send', 'pageview');
-    </script>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KGXPMMB');</script>
+<!-- End Google Tag Manager -->
+
 
 
 </head>
@@ -48,7 +48,7 @@ $result = $connection->query('SELECT * FROM `opdrachten`');
             <nav class="nav-bar">
 
                 <div class="brand">
-                    <a href="index-engels.php"><img src="img/logo.png" alt="logo" class="logo"></a>
+                    <a href="index.php"><img src="img/logo.png" alt="logo" class="logo"></a>
                 </div>
                 <div class="nav-list">
                     <div class="hamburger">
--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $result = $connection->query('SELECT * FROM `opdrachten`');
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 
 <head>
     <meta charset="UTF-8">
@@ -20,28 +20,21 @@ $result = $connection->query('SELECT * FROM `opdrachten`');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="HTML, CSS, JavaScript">
     <meta name="author" content="Halil Dogan">
+    <meta name="discription" content="Web-development">
     <title>Welkom bij uw web-design website!</title>
     <link rel="stylesheet" href="css/style.css">
     <script src="js/main.js" defer></script>
     <script src=".vscode/settings.json" defer></script>
     <script src="js/speak.js" defer></script>
+    <script src="js/analystic.js" defer></script>
 
-    <script>
-        (function(i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-            i[r] = i[r] || function() {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
-            a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0];
-            a.async = 1;
-            a.src = g;
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-        ga('create', 'UA-XXXXX-Y', 'auto');
-        ga('send', 'pageview');
-    </script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KGXPMMB');</script>
+<!-- End Google Tag Manager -->
 
 
 </head>
@@ -148,7 +141,7 @@ $result = $connection->query('SELECT * FROM `opdrachten`');
                                 </p>
                             </div>
                             <div class="project-img">
-                                <a href=""> <img src="img/<?php echo $row['foto']; ?>" alt="Img van mijn project van periode 2 over museum scheepvaart."></a>
+                                <a href="https://33289.hosts1.ma-cloud.nl/header/"> <img src="img/<?php echo $row['foto']; ?>" alt="Img van mijn project van periode 2 over museum scheepvaart."></a>
                             </div>
                         
                         </div>
